e45413f0221b5f365d41c69a7504fc9594a5f3a0
DmitrievYK <fireworcks@gmail.com>
2023-07-30 15:16:03 +0300

    add example 007 magic - explain how to

--- example007_magic/Program.cs
+++ example007_magic/Program.cs
@@ -2,10 +2,12 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
+// Блок с позицией изначального треугольника - Block with the position of the start triangle
 int xa = 40, ya = 1,
 xb = 1, yb = 30,
 xc = 80, yc = 30;
 
+// Блок кода рисует знаком "+" в консоли конкретную позицию треугольника - The code block draws a specific triangle position in the console with a "+" sign
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
 
@@ -15,22 +17,23 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = xb;
-int count = 0;
-while(count < 10000)
+int x = xa, y = xb; // задаем точку и в нее кладем координату 
+int count = 0; // счетчик повторений операций - repetition counter
+while(count < 10000) // счетчик пока не выполним условие
 {
-    int what = new Random().Next(0, 3); //[0;3) 0 1 2
-    if(what == 0)
+    int what = new Random().Next(0, 3); //[0;3) 0 1 2 - случайное чисто, но у нас всего три точки
+    if(what == 0) // для первой точки
     {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
+        x = (x + xa) / 2; // поиск центр отрезка
+        y = (y + ya) / 2; // поиск центр отрезка
     }
-    if (what == 1)
+    if (what == 1) // для второй точки
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
-    if (what == 2)
+    if (what == 2) // для третьей точки
+
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
